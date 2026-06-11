5e868dc4c7ccc0ff3b5f5bfad4056f704a7b9ea6
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-06-11 18:15:27 +0700

    Fix search method to correctly update parent and current node during traversal

--- tugas.cpp
+++ tugas.cpp
@@ -65,5 +65,13 @@ public:
         parent = NULL;
 
         while ((currentnode !=NULL) && (currentnode->info != element))
+        {
+            parent = currentnode;
+
+            if (element<currentnode->info)
+                currentnode = currentnode->leftchild;
+            else
+                currentnode = currentnode->rightchild;
+        }
     }
 }
