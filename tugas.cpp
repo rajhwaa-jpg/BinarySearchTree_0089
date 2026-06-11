924e7b0293250035ed633a3428dc25a4087b7b84
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-06-11 18:22:06 +0700

    Add preorder traversal method to binarytree class

--- tugas.cpp
+++ tugas.cpp
@@ -88,4 +88,18 @@ public:
             inorder(ptr->rightchild);
         }
     }
+    void preorder(node *ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "tree is empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            cout << ptr->info << "";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+        }
+    }
 }
