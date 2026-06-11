fc449889bc946cab6ed5f61e2499e5d453a9594b
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-06-11 18:20:41 +0700

    Fix inorder traversal method to handle empty tree case

--- tugas.cpp
+++ tugas.cpp
@@ -81,5 +81,11 @@ public:
             cout << "tree is empty" << endl;
             return;
         }
+        if (ptr !=NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << "";
+            inorder(ptr->rightchild);
+        }
     }
 }
